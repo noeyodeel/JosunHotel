5f37b3c23a6bbed91032be3710121d9bf6d06721
min01010 <105161216+min01010@users.noreply.github.com>
2022-05-25 22:41:25 +0900

    헤더 푸터 수정 by박민지
    activity 제외 수정 필요

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -1,6 +1,5 @@
 window.onscroll = function () {
   scrollFunction();
-  scrollFunction2();
 };
 // 스크롤 시 헤더 바꾸는 함수
 function scrollFunction() {
@@ -14,69 +13,33 @@ function scrollFunction() {
     document.getElementById("nav_bar").style.top = "-300px"; //스크롤이 내려가지 않았을 때는 -300에 위치시켜서 숨김
   }
 }
-function scrollFunction2() {
-  if (
-    //스크롤 100만큼 내렸을 때
-    document.body.scrollTop < 100 ||
-    document.documentElement.scrollTop < 100
-  ) {
-    // $("#nav_bar").css("display", "none");
-  }
-}
 
-$(document).ready(function () {
+$(function () {
   //스크롤 마지막인식해서 푸터 뜨게함
-  $(".wrapper").scroll(function () {
-    var scrollTop = $(this).scrollTop();
-    var innerHeight = $(this).innerHeight();
-    var scrollHeight = $(this).prop("scrollHeight");
+  // $(".wrapper").scroll(function () {
+  //   var scrollTop = $(this).scrollTop();
+  //   var innerHeight = $(this).innerHeight();
+  //   var scrollHeight = $(this).prop("scrollHeight");
 
-    if (scrollTop + innerHeight >= scrollHeight) {
-      $("footer").attr("disabled", false);
-    } else {
-      $("footer").attr("disabled", true);
-    }
-  });
-  //메뉴 슬라이드
-  $(".mainbuilding_list_ > li > button").click(function (e) {
-    e.stopPropagation();
-    $(this).next($(".main_roomlist_")).slideToggle("fast");
-  });
-  $(".hillbuilding_list_ > li > button").click(function (e) {
-    e.stopPropagation();
-    $(this).next($(".hill_roomlist_")).slideToggle("fast");
-  });
-  //전체 네비게이션 클릭시 슬라이드- 방법1
-  // $(".dropdown_new> div>button").click(function (e) {
-  //   // e.stopPropagation();
-  //   $(this).next($(".dc_new")).slideToggle("fast");
-  // });
-
-  //전체 네비게이션 클릭시 슬라이드- 방법2
-  // $(".dm-new").click(function () {
-  //   if ($(this).hasClass("active")) {
-  //     $(".dc_new").css("display", "none");
-  //     $(this).removeClass();
-  //     $(".gnb_new").css("height", "30px");
+  //   if (scrollTop + innerHeight >= scrollHeight) {
+  //     $("footer").attr("disabled", false);
   //   } else {
-  //     $(this).addClass("active");
-  //     $(".dc_new").css("display", "block");
-  //     $(".gnb_new").css("height", "590px");
+  //     $("footer").attr("disabled", true);
   //   }
   // });
 
-  // 버튼 클릭 시 스타일 변경
-  $(".mainbuilding_list_ > li > button").focus(function () {
-    $(this).addClass("selec");
-  });
-  $(".mainbuilding_list_ > li > button").blur(function () {
-    $(this).removeClass("selec");
+  //메뉴 슬라이드
+  $(document).on("click", ".slide_btn", function (e) {
+    // $(".slide_btn").click(function (e) 쓰지 말것 [오류남]
+    e.stopPropagation();
+    $(this).next($(".main_roomlist_")).slideToggle("fast");
   });
 
-  $(".hillbuilding_list_ > li > button").focus(function () {
+  //버튼 클릭 시 스타일 변경
+  $(document).on("focus", ".slide_btn", function () {
     $(this).addClass("selec");
   });
-  $(".hillbuilding_list_ > li > button").blur(function () {
+  $(document).on("blur", ".slide_btn", function () {
     $(this).removeClass("selec");
   });
 });
