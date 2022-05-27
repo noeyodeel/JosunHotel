17f07f1035308de3c5bc6139c1c99e728fcdff95
jumpinghill <jumpinghill@naver.com>
2022-05-27 15:55:14 +0900

    Revert "0527 deluxe.html,css,js수정 by주형"
    This reverts commit c4473999b3c459a41157add56b95c84189d48c65.

--- js/deluxe.js
+++ js/deluxe.js
@@ -33,42 +33,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
   loveBtnWrap.addEventListener("click", changeBtn);
   // 하트 버튼 색 변경 끝
-
-  // 예약하기 버튼 호버시 background색 변경 시작
-  document.querySelector(".scroll_top").addEventListener("click", function () {
-    const location = document.querySelector(".scroll").offsetTop;
-    const menuHeight = document.querySelector(".menu_height").offsetHeight;
-    window.scrollTo({ top: location - menuHeight, behavior: "smooth" });
-  });
-  // 예약하기 버튼 호버시 background색 변경 끝
-
-  // 메인이미지 슬라이드 시작
-  const slides = document.querySelector(".deluxe_01"); //전체 슬라이드 컨테이너
-  const slideImg = document.querySelectorAll(".deluxe_01 li"); //모든 슬라이드들
-  let currentIdx = 0; //현재 슬라이드 index
-  const slideCount = slideImg.length; // 슬라이드 개수
-  const prev = document.querySelector(".btnPrev"); //이전 버튼
-  const next = document.querySelector(".btnNext"); //다음 버튼
-  const slideWidth = 1400; //한개의 슬라이드 넓이
-
-  //전체 슬라이드 컨테이너 넓이 설정
-  slides.style.width = slideWidth * slideCount + "px";
-
-  function moveSlide(num) {
-    slides.style.left = -num * 1400 + "px";
-    currentIdx = num;
-  }
-
-  // 왼쪽 버튼
-  prev.addEventListener("click", function () {
-    if (currentIdx !== 0) moveSlide(currentIdx - 1);
-  });
-
-  // 오른쪽 버튼
-  next.addEventListener("click", function () {
-    if (currentIdx !== slideCount - 1) {
-      moveSlide(currentIdx + 1);
-    }
-  });
-  // 메인이미지 슬라이드 끝
 });
