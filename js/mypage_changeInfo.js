79d1e63341993e49a2dede93d3d0c93880548275
clouououd <wjdrhks02050@gmail.com>
2022-06-02 09:08:55 +0900

    220602 mypage 김정관
    회원정보수정(html,css,js,image), 회원탈퇴(html,css,js,image)

---
+++ js/mypage_changeInfo.js
@@ -0,0 +1,35 @@
+// select
+const selectOpenClose = (li) => {
+  if (!$(li + " button.select").hasClass("open")) {
+    $(li + " button.select").addClass("open");
+    $(li + " .option").addClass("openOption");
+  } else {
+    $(li + " button.select").removeClass("open");
+    $(li + " .option").removeClass("openOption");
+  }
+};
+
+$(".nationalityArea button.select").click(() => {
+  selectOpenClose(".nationalityArea");
+});
+$(".emailArea button.select").click(() => {
+  selectOpenClose(".emailArea");
+});
+
+// 약관동의
+const agreeOpenClose = (li) => {
+  if (!$(li + "> div > button").hasClass("openAgreeBtn")) {
+    $(li + "> div > button").addClass("openAgreeBtn");
+    $(li + "> div > button").text("상세내용 닫기");
+    $(li + " .detailArea").addClass("openAgree");
+  } else {
+    $(li + "> div > button").removeClass("openAgreeBtn");
+    $(li + "> div > button").text("상세내용 열기");
+    $(li + " .detailArea").removeClass("openAgree");
+  }
+};
+
+$(".agree1 > div > button").click(() => {
+  agreeOpenClose(".agree1");
+  console.log($(".agree1" + " .detailArea"));
+});
