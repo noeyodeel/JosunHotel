1620c20cdad5f04dd97dab3282f21a60d5672688
jumpinghill <jumpinghill@naver.com>
2022-06-09 02:25:07 +0900

    0609 deluxe.css,js수정 by주형
    deluxe.css,js수정

--- js/deluxe.js
+++ js/deluxe.js
@@ -44,33 +44,33 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // 메인이미지 슬라이드 시작
-  const slides = document.querySelector(".deluxe_01"); //전체 슬라이드 컨테이너
-  const slideImg = document.querySelectorAll(".deluxe_01 li"); //모든 슬라이드들
-  let currentIdx = 0; //현재 슬라이드 index
-  const slideCount = slideImg.length; // 슬라이드 개수
-  const prev = document.querySelector(".btnPrev"); //이전 버튼
-  const next = document.querySelector(".btnNext"); //다음 버튼
-  const slideWidth = 1400; //한개의 슬라이드 넓이
+  // const slides = document.querySelector(".deluxe_01"); //전체 슬라이드 컨테이너
+  // const slideImg = document.querySelectorAll(".deluxe_01 li"); //모든 슬라이드들
+  // let currentIdx = 0; //현재 슬라이드 index
+  // const slideCount = slideImg.length; // 슬라이드 개수
+  // const prev = document.querySelector(".btnPrev"); //이전 버튼
+  // const next = document.querySelector(".btnNext"); //다음 버튼
+  // const slideWidth = 1400; //한개의 슬라이드 넓이
 
-  //전체 슬라이드 컨테이너 넓이 설정
-  slides.style.width = slideWidth * slideCount + "px";
+  // //전체 슬라이드 컨테이너 넓이 설정
+  // slides.style.width = slideWidth * slideCount + "px";
 
-  function moveSlide(num) {
-    slides.style.left = -num * 1400 + "px";
-    currentIdx = num;
-  }
+  // function moveSlide(num) {
+  //   slides.style.left = -num * 1400 + "px";
+  //   currentIdx = num;
+  // }
 
-  // 왼쪽 버튼
-  prev.addEventListener("click", function () {
-    if (currentIdx !== 0) moveSlide(currentIdx - 1);
-  });
+  // // 왼쪽 버튼
+  // prev.addEventListener("click", function () {
+  //   if (currentIdx !== 0) moveSlide(currentIdx - 1);
+  // });
 
-  // 오른쪽 버튼
-  next.addEventListener("click", function () {
-    if (currentIdx !== slideCount - 1) {
-      moveSlide(currentIdx + 1);
-    }
-  });
+  // // 오른쪽 버튼
+  // next.addEventListener("click", function () {
+  //   if (currentIdx !== slideCount - 1) {
+  //     moveSlide(currentIdx + 1);
+  //   }
+  // });
   // 메인이미지 슬라이드 끝
 
   // 객실 어메니티 버튼 클릭시 리스트 시작
@@ -205,4 +205,114 @@ document.addEventListener("DOMContentLoaded", function () {
       closet_btn.style.borderBottom = "none";
     });
   // 객실 어메니티 버튼 클릭시 리스트 끝
+
+  (function () {
+    const slideList = document.querySelector(".deluxe_01"); // Slide parent dom
+    const slideContents = document.querySelectorAll(".common_deluxeImg"); // each slide dom
+    const slideBtnNext = document.querySelector(".btnNext"); // next button
+    const slideBtnPrev = document.querySelector(".btnPrev"); // prev button
+    // const pagination = document.querySelector(".slide_pagination");
+    const slideLen = slideContents.length; // slide length
+    const slideWidth = 1400; // slide width
+    const slideSpeed = 300; // slide speed
+    const startNum = 0; // initial slide index (0 ~ 4)
+
+    slideList.style.width = slideWidth * (slideLen + 2) + "px";
+
+    // Copy first and last slide
+    let firstChild = slideList.firstElementChild;
+    let lastChild = slideList.lastElementChild;
+    let clonedFirst = firstChild.cloneNode(true);
+    let clonedLast = lastChild.cloneNode(true);
+
+    // Add copied Slides
+    slideList.appendChild(clonedFirst);
+    slideList.insertBefore(clonedLast, slideList.firstElementChild);
+
+    // Add pagination dynamically
+    // let pageChild = "";
+    // for (var i = 0; i < slideLen; i++) {
+    //   pageChild += '<li class="dot';
+    //   pageChild += i === startNum ? " dot_active" : "";
+    //   pageChild += '" data-index="' + i + '"><a href="#"></a></li>';
+    // }
+    // pagination.innerHTML = pageChild;
+    // const pageDots = document.querySelectorAll(".dot"); // each dot from pagination
+
+    slideList.style.transform =
+      "translate3d(-" + slideWidth * (startNum + 1) + "px, 0px, 0px)";
+
+    let curIndex = startNum; // current slide index (except copied slide)
+    let curSlide = slideContents[curIndex]; // current slide dom
+    curSlide.classList.add("slide_active");
+
+    /** Next Button Event */
+    slideBtnNext.addEventListener("click", function () {
+      if (curIndex <= slideLen - 1) {
+        slideList.style.transition = slideSpeed + "ms";
+        slideList.style.transform =
+          "translate3d(-" + slideWidth * (curIndex + 2) + "px, 0px, 0px)";
+      }
+      if (curIndex === slideLen - 1) {
+        setTimeout(function () {
+          slideList.style.transition = "0ms";
+          slideList.style.transform =
+            "translate3d(-" + slideWidth + "px, 0px, 0px)";
+        }, slideSpeed);
+        curIndex = -1;
+      }
+      curSlide.classList.remove("slide_active");
+      // pageDots[curIndex === -1 ? slideLen - 1 : curIndex].classList.remove(
+      //   "dot_active"
+      // );
+      curSlide = slideContents[++curIndex];
+      curSlide.classList.add("slide_active");
+      // pageDots[curIndex].classList.add("dot_active");
+    });
+
+    /** Prev Button Event */
+    slideBtnPrev.addEventListener("click", function () {
+      if (curIndex >= 0) {
+        slideList.style.transition = slideSpeed + "ms";
+        slideList.style.transform =
+          "translate3d(-" + slideWidth * curIndex + "px, 0px, 0px)";
+      }
+      if (curIndex === 0) {
+        setTimeout(function () {
+          slideList.style.transition = "0ms";
+          slideList.style.transform =
+            "translate3d(-" + slideWidth * slideLen + "px, 0px, 0px)";
+        }, slideSpeed);
+        curIndex = slideLen;
+      }
+      curSlide.classList.remove("slide_active");
+      // pageDots[curIndex === slideLen ? 0 : curIndex].classList.remove(
+      //   "dot_active"
+      // );
+      curSlide = slideContents[--curIndex];
+      curSlide.classList.add("slide_active");
+      // pageDots[curIndex].classList.add("dot_active");
+    });
+
+    /** Pagination Button Event */
+    // let curDot;
+    // Array.prototype.forEach.call(pageDots, function (dot, i) {
+    //   dot.addEventListener("click", function (e) {
+    //     e.preventDefault();
+    //     curDot = document.querySelector(".dot_active");
+    //     curDot.classList.remove("dot_active");
+
+    //     curDot = this;
+    //     this.classList.add("dot_active");
+
+    //     curSlide.classList.remove("slide_active");
+    //     curIndex = Number(this.getAttribute("data-index"));
+    //     curSlide = slideContents[curIndex];
+    //     curSlide.classList.add("slide_active");
+    //     slideList.style.transition = slideSpeed + "ms";
+    //     slideList.style.transform =
+    //       "translate3d(-" + slideWidth * (curIndex + 1) + "px, 0px, 0px)";
+    //   });
+    // });
+  })();
 });
