225cdbdd910154878bf4812f0227ea97a9d6361a
doyeonle <sko8044@gmail.com>
2022-06-19 21:02:04 +0900

    0619 join, login 수정 by 이도연

--- js/login.js
+++ js/login.js
@@ -1,41 +1,3 @@
-// const agreeOpenClose = (li) => {
-// 	if (!$(li + "> div > button").hasClass("openAgreeBtn")) {
-// 		$(li + "> div > button").addClass("openAgreeBtn");
-// 		$(li + "> div > button").text("상세내용 닫기");
-// 		$(li + " .detailArea").addClass("openAgree");
-// 	} else {
-// 		$(li + "> div > button").removeClass("openAgreeBtn");
-// 		$(li + "> div > button").text("상세내용 열기");
-// 		$(li + " .detailArea").removeClass("openAgree");
-// 	}
-// };
-
-// $(".agree1 > div > button").click(() => {
-// 	agreeOpenClose(".agree1");
-// 	console.log($(".agree1" + " .detailArea"));
-// });
-
-// const agreeOpen = (li) => {
-// 	if (!$(li).hasClass("toggleOn")) {
-// 		$(li ).addClass("toggleOn");
-// 		$(li + "> button > span").text("상세내용 닫기");
-// 		$(li + " .agree").addClass("toggleCont");
-// 		$(li + " > .agree > .box").addClass("toggleInner");
-// 		// $(li + "> .agree ").Attr("style","display:block");
-// 		// .height(curHeight).animate({
-// 		// 	height: autoHeight
-// 		// }, 500);
-// 	} else {
-// 		$(li).removeClass("toggleOn");
-// 		$(li + "> button > span").text("상세내용 열기");
-// 		$(li + " .agree").removeClass("toggleCont");
-// 		$(li + "> .agree > .box").removeClass("toggleInner");
-// 		$(li + "> .agree > .box >notiTxt").removeClass(");
-// 		$(li + "> .agree").removeAttr("style");
-
-// 	}
-// };
-
 $("button[name='btnToggle']").click(() => {
 	// agreeOpen(".toggle");
 	$('.agree').slideToggle();
@@ -49,49 +11,48 @@ $("button[name='btnToggletwo']").click(() => {
 	console.log("dk");
 });
 
-// 체크선택
-// 체크 선택
-// $(".frm > label").click(() => {
-// 	$(".frm > label").Attr("style", "background-position: 0 -13px;");
-// });
-
-function is_checked() {
-
-	// 1. checkbox element를 찾습니다.
-	const checkbox = document.getElementById('frmChk01');
-
-	// 2. checked 속성을 체크합니다.
-	const is_checked = checkbox.checked;
-
-	// 3. 결과를 출력합니다.
-	if(is_checked == true){
-
-		document.getElementById('frmChk01').style.backgroundPosition="top right";
-		console.log("ok");
+// select
+const selectOpenClose = (li) => {
+	let selectBtn = document.querySelector(li + " button.select");
+	let option = document.querySelector(li + " .option");
+	if (!selectBtn.classList.contains("open")) {
+		selectBtn.classList.add("open");
+		option.classList.add("openOption");
+	} else {
+		selectBtn.classList.remove("open");
+		option.classList.remove("openOption");
 	}
+};
 
-}
+$(document).on("click", ".nationalityArea button.select", function () {
+	selectOpenClose(".nationalityArea");
+});
+
+$(".nationalityArea .optionList").click((e) => {
+	console.log("click");
+	$("#select").val(e.currentTarget.innerText).prop("selected", true);
+	$("#selectBtn").text(e.currentTarget.innerText);
+	document.querySelector("#selectBtn").classList.remove("open");
+	document.querySelector("#option").classList.remove("openOption");
+});
+
+$(document).on("click", ".emailArea button.select", function () {
+	selectOpenClose(".emailArea");
+});
+$(document).on("click", ".HotelArea button.select", function () {
+	selectOpenClose(".HotelArea");
+});
 
 
-
-
-
-
-
-
-
-
-
-// $('.btnToggle').on('click', function () {
-// 	var open = $(this).next('.toggleCont').height();
-// 	var el = $('.toggleCont'),
-// 		curHeight = el.height(),
-// 		autoHeight = el.css('height', 'auto').height();
-// 	if (open === 0) {
-// 		el.height(curHeight).animate(
-// 			{height: autoHeight}, 500);
-// 	} else {
-// 		el.animate(
-// 			{height: 0}, 500);
-// 	}
-// });
+$(".emailArea .optionList").click((e) => {
+	$("#selectEmail").val(e.currentTarget.innerText).prop("selected", true);
+	$("#selectEmailBtn").text(e.currentTarget.innerText);
+	document.querySelector("#selectEmailBtn").classList.remove("open");
+	document.querySelector("#optionEmail").classList.remove("openOption");
+});
+$(".HotelArea .optionList").click((e) => {
+	$("#selectHotel").val(e.currentTarget.innerText).prop("selected", true);
+	$("#selectHotelBtn").text(e.currentTarget.innerText);
+	document.querySelector("#selectHotelBtn").classList.remove("open");
+	document.querySelector("#optionHotel").classList.remove("openOption");
+});
