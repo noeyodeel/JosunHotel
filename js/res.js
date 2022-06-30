a27ba10d90e9aedab1fb297e25b451af6c89e4ff
cyndii0502 <90389093+cyndii20@users.noreply.github.com>
2022-07-01 07:40:31 +0900

    0701 7:40 서정연
    datepicker single 구현

--- js/res.js
+++ js/res.js
@@ -221,3 +221,36 @@ $(function () {
     PrevSlide();
   });
 });
+
+// datepicker
+$(function () {
+  $("#diningCal").datepicker({
+    inline: true,
+    showOtherMonths: true,
+    dayNamesMin: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    monthNames: [
+      ". 01",
+      ". 02",
+      ". 03",
+      ". 04",
+      ". 05",
+      ". 06",
+      ". 07",
+      ". 08",
+      ". 09",
+      ". 10",
+      ". 11",
+      ". 12",
+    ],
+    showMonthAfterYear: true,
+  });
+});
+
+function off() {
+  $(".rightCont.alert .alertText").hide();
+  document.getElementById("alert").classList.remove("alert");
+}
+
+$("").click(function () {
+  var id_check = $(this).attr("id");
+});
